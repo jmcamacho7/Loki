9f454b72d579055e6bf9b9547ab34b38dda0cb66
jmcamacho7 <camachosanchezjm@gmail.com>
2023-01-13 20:09:07 +0100

    Día 13/01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, NgModule} from '@angular/core';
+import {InicioComponent} from "./inicio/inicio.component";
+import {PruebaComponent} from "./prueba/prueba.component";
 
 @Component({
   selector: 'app-root',
@@ -14,4 +16,7 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class AppModule {}
+
+
+
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {IonicModule} from "@ionic/angular";
+import { PruebaComponent } from './prueba/prueba.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeroesComponent } from './heroes/heroes.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent,
+    InicioComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule
+    RouterModule.forRoot([
+      {path: 'inicio', component: InicioComponent},
+      {path: 'inicio-sesion', component: PruebaComponent},
+    ]),
+    IonicModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
