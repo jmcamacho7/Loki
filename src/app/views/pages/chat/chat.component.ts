81922ae0ace82cde0220f75bce5bca3c0a64f37e
jmcamacho7 <camachosanchezjm@gmail.com>
2023-03-07 23:47:58 +0100

    ChatGPT

--- src/app/views/pages/chat/chat.component.ts
+++ src/app/views/pages/chat/chat.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Router} from "@angular/router";
+import { Configuration, OpenAIApi } from "openai";
+
+
+
+
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
+
 export class ChatComponent {
-  constructor(private http: HttpClient, private router: Router) {}
+
+  openai: OpenAIApi;
+
+  constructor(private http: HttpClient, private router: Router) {
+    const configuracion = new Configuration({
+      apiKey: '',
+    });
+    this.openai = new OpenAIApi(configuracion);
+  }
+
+  async chatInteligente(texto: any) {
+    const completion = await this.openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: texto }],
+    });
+
+    console.log(completion)
+    // @ts-ignore
+   const respuesta = completion.choices.message.content;
+   this.enviarMensajeGPT(respuesta)
+  }
 
   listaUsuarios: any;
   conversacion: any;
@@ -22,12 +48,8 @@ export class ChatComponent {
     'foto':''
   }
 
-  chatGPT={
-    "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": ""}]
-  }
-
   gptMensaje: any;
+
 
 
   ngOnInit() {
@@ -78,25 +100,7 @@ export class ChatComponent {
   refreshPage() {
     location.reload();
   }
-  refrescar(id:any){
-    const params = new HttpParams().set('usuario_id', id)
-    this.idConver = id;
 
-    const token: string | null = localStorage.getItem('token')
-    const headers = new HttpHeaders({'apikey': token!})
-
-    this.http.get("/api/chat/chatUsuarioUsuario", {headers, params})
-      .subscribe(
-        resultado => {
-          // @ts-ignore
-          this.conversacion = resultado.sort((a: any, b: any) => {
-            let fechaA = new Date(a.fecha).getTime();
-            let fechaB = new Date(b.fecha).getTime();
-            return fechaA - fechaB;
-          });;
-        }
-      );
-  }
 
   cerrarChat(){
     const todoschat:any = document.getElementById('todoschat');
@@ -119,26 +123,32 @@ export class ChatComponent {
     this.http.post('http://localhost:8000/api/chat/enviarMensaje', body, {headers: headers})
       // @ts-ignore
       .subscribe(() => {
+        if (idUsuario === 10) {
+          this.chatInteligente(this.mensaje.texto)
+        }
+        else{
+          this.refreshPage()
+          this.mensaje.texto = ''
+        }
+      });
+
+  }
+  enviarMensajeGPT(text:any){
+    const token: string | null = localStorage.getItem('token')
+    console.log(token)
+    const headers = new HttpHeaders({'apikey': token!})
+    const body = JSON.stringify({
+      'usuarioIdEmisor':10,
+      'usuarioIdReceptor': 1,
+      'texto':text,
+      'foto':''})
+    console.log(body)
+    this.http.post('http://localhost:8000/api/chat/enviarMensajeChatGPT', body, {headers: headers})
+      // @ts-ignore
+      .subscribe(() => {
         this.refreshPage()
         this.mensaje.texto = ''
       });
-    if (idUsuario === 10){
-      this.chatGPTenviar(this.mensaje.texto)
-    }
-
   }
 
-  chatGPTenviar(texto:any){
-    const body = JSON.stringify({
-      "model": "gpt-3.5-turbo",
-      "messages": [{"role": "user", "content": texto}]})
-    this.http.post('https://chatgpt-api.shn.hk/v1/', body)
-      // @ts-ignore
-      .subscribe(
-        resultado => {
-          // @ts-ignore
-          this.gptMensaje = resultado
-          console.log(this.gptMensaje)
-          });
-  }
 }
